5fc772c170a88323b5b540c5d0a60f6d9cbc466a
paula <paula@LAPTOP-TUREMBCI>
2026-06-25 01:10:10 -0500

    VENTANAS-INTERFAZ GRAFICA

--- src/main/java/modulo/vista/Main.java
+++ src/main/java/modulo/vista/Main.java
@@ -1,5 +1,7 @@
 package modulo.vista;
 
+import modulo.vistas.VentanaLogin;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,8 +12,26 @@ package modulo.vista;
  * @author paula
  */
 public class Main {
-    public static void main(String[] args) {
-        VistaConsola sistema = new VistaConsola();
-        sistema.iniciarSistema();
+ public static void main(String[] args) {
+        /* Configuración del Look and Feel para mantener consistencia visual */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Despliegue síncrono en el hilo de despacho de eventos de Swing (EDT) */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // Instanciamos la ventana de Login y la hacemos visible
+                new VentanaLogin().setVisible(true);
+            }
+        });
     }
 }
